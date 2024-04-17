aedd6f64dec6aaf2e0bca34fea1d01035bd93622
labram13 <labram@uw.edu>
2024-04-16 22:03:54 -0700

    added player one functionality and player one loss text

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var playerOneHealth = 20
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        playerLoseLabel.isHidden = true;
+        playerOneHealthLabel.text = String(playerOneHealth)
     }
-
-
-}
+    @IBOutlet weak var playerOneHealthLabel: UILabel!
+    @IBOutlet weak var playerLoseLabel: UILabel!
+    @IBAction func HandleAddOneHealth(_ sender: Any) {
+        playerOneHealth += 1
+        playerOneHealthLabel.text = String(playerOneHealth)
+    }
+    @IBAction func HandleMinusOneHealth(_ sender: Any) {
+        playerOneHealth -= 1
+        playerOneHealthLabel.text = String(playerOneHealth)
+    }
+    
+    @IBAction func HandleAddFiveHealth(_ sender: Any) {
+        playerOneHealth += 5
+        playerOneHealthLabel.text = String(playerOneHealth)
+    }
+    
+    @IBAction func HandleMinusFiveHealth(_ sender: Any) {
+        playerOneHealth -= 5
+        playerOneHealthLabel.text = String(playerOneHealth)
+        if playerOneHealth <= 0 {
+            playerLoseLabel.text = "Player One Loses!"
+            playerLoseLabel.isHidden = false
+        }
+    }
+    
+    }
+                                           
 
